def237ba5d5944f06406b9288767021d7bcfcf79
adenugbamichael <adenugbamicke@gmail.com>
2024-07-10 10:35:50 +0100

    Implementing the HTTP API clients

--- src/data/dataStore.ts
+++ src/data/dataStore.ts
@@ -1,10 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import { reducer as selectionsReducer, addToOrder } from "./selectionSlice"
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { productsApi, useGetProductsQuery } from "./storeApis"
 export const dataStore = configureStore({
   reducer: {
     selections: selectionsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 })
 export type AppDispatch = typeof dataStore.dispatch
 export type RootState = ReturnType<typeof dataStore.getState>
@@ -12,4 +16,7 @@ export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export const reducers = {
   addToOrder,
+}
+export const queries = {
+  useGetProductsQuery,
 }
